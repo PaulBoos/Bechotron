e5ea5c69a614e8ad39daa751e517d01e92d13cad
GelberBecher <34918079+GelberBecher@users.noreply.github.com>
2022-05-24 23:51:10 +0200

    MIDI Push #1

--- src/main/java/Modules/MIDI/MIDItest.java
+++ src/main/java/Modules/MIDI/MIDItest.java
@@ -6,23 +6,26 @@ import java.util.*;
 
 public class MIDItest {
 	
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws MidiUnavailableException {
 		
 		MIDItest player = new MIDItest();
 		
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the instrument to be played");
-		int instrument = in.nextInt();
-		System.out.println("Enter the note to be played");
-		int note = in.nextInt();
+//		Scanner in = new Scanner(System.in);
+//		System.out.println("Enter the instrument to be played");
+//		int instrument = in.nextInt();
+//		System.out.println("Enter the note to be played");
+//		int note = in.nextInt();
 		
-		player.setUpPlayer(instrument, note);
-	}
-	
-	public void setUpPlayer(int instrument, int note)
-	{
 		
+		MidiDevice.Info[] info = MidiSystem.getMidiDeviceInfo();
+		MidiDevice device;
+		for(MidiDevice.Info i: info)
+			if(i.getName().equals("Gervill"))
+				device = MidiSystem.getMidiDevice(i);
+		
+		
+		
+		/*
 		try {
 			
 			Sequencer sequencer = MidiSystem.getSequencer();
@@ -39,6 +42,13 @@ public class MIDItest {
 			// Add a note off event with specified note
 			track.add(makeEvent(128, 1, note, 100, 4));
 			
+			track.add(makeEvent(144, 1, note+10, 100, 5));
+			track.add(makeEvent(128, 1, note+10, 100, 8));
+			track.add(makeEvent(144, 1, note+20, 100, 9));
+			track.add(makeEvent(128, 1, note+20, 100, 12));
+			track.add(makeEvent(144, 1, note+30, 100, 13));
+			track.add(makeEvent(128, 1, note+30, 100, 16));
+			
 			sequencer.setSequence(sequence);
 			sequencer.start();
 			
@@ -53,10 +63,10 @@ public class MIDItest {
 		catch (Exception ex) {
 			
 			ex.printStackTrace();
-		}
+		}*/
 	}
 	
-	public MidiEvent makeEvent(int command, int channel,
+	public static MidiEvent makeEvent(int command, int channel,
 							   int note, int velocity, int tick)
 	{
 		

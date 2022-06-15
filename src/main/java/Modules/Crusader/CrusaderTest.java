91b607c315f6f3e6126d064b4f4ed6fc90353612
GelberBecher <34918079+GelberBecher@users.noreply.github.com>
2022-06-16 00:44:58 +0200

    Train sync #6

--- src/main/java/Modules/Crusader/CrusaderTest.java
+++ src/main/java/Modules/Crusader/CrusaderTest.java
@@ -8,12 +8,13 @@ public class CrusaderTest {
 	
 	
 	public static void main(String[] args) throws JacksonException {
-		String xml = "<Person><name>Testperson</name><prenom>Peter</prenom><nickname>that dude</nickname></Person>";
+		String xml = "<Person><name>Testperson</name><prenom>Peter</prenom><nickname>that dude</nickname><id>10</id></Person>";
 		Person d = Person.deserialize(xml);
-//		Person p = new Person("Testperson", "Peter", "that dude");
+		Person p = new Person("Testperson", "Peter", "that dude", 1);
 		System.out.println(d);
+		System.out.println(p);
 		System.out.println(d.serialize());
-		System.out.println(d.getFullName());
+		//System.out.println(d.getFullName());
 	}
 	
 }
--- src/main/java/Modules/Crusader/Entities/Person.java
+++ src/main/java/Modules/Crusader/Entities/Person.java
@@ -1,5 +1,6 @@
 package Modules.Crusader.Entities;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -16,36 +17,47 @@ public class Person implements CrusaderEntity {
 	@JacksonXmlProperty
 	String name, prenom, nickname;
 	
-	@ConstructorProperties({"name", "prenom", "nickname"})
-	public Person(String name, String prenom, String nickname) {
+	@JacksonXmlProperty
+	int id;
+	
+	@ConstructorProperties({"name", "prenom", "nickname", "id"})
+	public Person(String name, String prenom, String nickname, int id) {
 		this.name = name;
 		this.prenom = prenom;
 		this.nickname = nickname;
+		this.id = id;
 	}
 	
-	public String serialize() {
-		return String.format("<Person><name>%s</name><prenom>%s</prenom><nickname>%s</nickname></Person>", this.name, this.prenom, this.nickname);
+	public String serialize() throws JsonProcessingException {
+		return new XmlMapper().writeValueAsString(this);
 	}
 	
 	public static Person deserialize(String input) throws JacksonException {
 		return new XmlMapper().readValue(input, Person.class);
 	}
-	
+	/*
 	public String getFullName() {
 		return String.format("%s \"%s\" %s", prenom, nickname, name);
 	}
+	*/
+	@JsonGetter
+	public String getNickname() {
+		return nickname;
+	}
 	
+	@JsonGetter
 	public String getName() {
 		return name;
 	}
 	
+	@JsonGetter
 	public String getPrenom() {
 		return prenom;
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("Character {\n| name=\"%s\",\n| prenom=\"%s\",\n| nickname=\"%s\" }", name, prenom, nickname);
+		return String.format("Character #%05d {\n| name=\"%s\",\n| prenom=\"%s\",\n\\ nickname=\"%s\" }", id, name, prenom, nickname);
 	}
 	
 }
